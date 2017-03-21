c9967f3fb75e63d002e700621866ec3afbc3bca9
Liusw <951450218@qq.com>
2017-03-21 14:47:39 +0800

    df

--- src/main/java/com/dudu/weixin/control/Info.java
+++ src/main/java/com/dudu/weixin/control/Info.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/")
 public class Info {
-    
     @Autowired
     private LianmengIntroducedService lianmengIntroducedService;
 
