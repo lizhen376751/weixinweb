02c1b9f4106b1576d16f5f939cfc6e07f2069cfc
lizhen <724577807@qq.com>
2017-03-27 19:29:03 +0800

    格式验证

--- src/main/java/com/dudu/weixin/service/BuyRecordService.java
+++ src/main/java/com/dudu/weixin/service/BuyRecordService.java
@@ -14,24 +14,44 @@ import java.util.List;
 //TODO 后期合为一个接口(商品列表,一个项目列表,技师名字,支付方式)进行返回
 @Service
 public class BuyRecordService {
+    /**
+     * 常量无意义
+     */
+    private final int num2 = 10;
 
-    //获取消费列表
+    /**
+     * @param lmCode 联盟code
+     * @param carNo  联盟卡号
+     * @param top    页数
+     * @return list集合
+     * @Title 获取消费列表
+     */
     public ArrayList getServiceListByLmcodeAndCarNo(String lmCode, String carNo, String top) {
         ArrayList list = new ArrayList();
-        int num = Integer.valueOf(top) * 10;
+        int num = Integer.valueOf(top) * num2;
         System.out.println(" getServiceListByLmcodeAndCarNo 下拉==top:" + top + "|");
         //TODO 后期调用接口
         return list;
     }
 
-    //获取支付方式
-    public String getZhiFuFangShi(String strId) throws Exception {
+    /**
+     * @param strId 不知道
+     * @return
+     * @throws Exception
+     */
+    public String getZhiFuFangShi(String strId) {
         String strName = "";
         //TODO 后期调用接口
         return strName;
     }
 
-    //获取项目列表
+    /**
+     * @param shopcode    店铺代码
+     * @param wxpingzheng wx凭证
+     * @return
+     * @Title 获取项目列表
+     */
+
     public ArrayList getItemAndSpList(String shopcode, String wxpingzheng) {
         ArrayList list = new ArrayList();
         //TODO 后期调用接口
@@ -42,10 +62,10 @@ public class BuyRecordService {
      * 根据shopcode、员工id获取用户名称(获取技师的名字)
      *
      * @param shopcode code
-     * @return
-     * @throws Exception
+     * @param code     code
+     * @return 字符串
      */
-    public String getSalerName(String shopcode, String code) throws Exception {
+    public String getSalerName(String shopcode, String code) {
         HashMap salerHash = new HashMap();
         String salerName = "";
         String[] codes = null;
@@ -76,9 +96,13 @@ public class BuyRecordService {
         return salerName;
     }
 
-
-
-
+    /**
+     *
+     * @param shopcode
+     * @param CarHaoPai
+     * @param top
+     * @return
+     */
     public ArrayList listRecord(String shopcode, String CarHaoPai, String top) {
         ArrayList list = new ArrayList();
         int num = Integer.valueOf(top) * 10;
@@ -86,14 +110,26 @@ public class BuyRecordService {
         //TODO 调用接口
         return list;
     }
-
+    /**
+     *
+     * @param shopcode
+     * @param CarHaoPai
+     * @param top
+     * @return
+     */
     public String getItemRecord(String shopcode, String wxpingzheng) {
         String strRecord = "";
         ArrayList list = new ArrayList();
         //TODO 后期调用接口
         return strRecord;
     }
-
+    /**
+     *
+     * @param shopcode
+     * @param CarHaoPai
+     * @param top
+     * @return
+     */
     public String getGoodsRecord(String shopcode, String wxpingzheng) {
         String strRecord = "";
         ArrayList list = new ArrayList();
@@ -101,8 +137,12 @@ public class BuyRecordService {
         return strRecord;
     }
 
-
-    public String getShopMoneyFlg(String shopcode) throws Exception {
+    /**
+     *
+     * @param shopcode shopcode
+     * @return
+     */
+    public String getShopMoneyFlg(String shopcode)  {
         String strFlg = "";
         //TODO 后期调用接口
         return strFlg;
@@ -112,10 +152,9 @@ public class BuyRecordService {
     /**
      * 根据店铺编码、单据编号获取商品明细及项目明细的维修员编码字符串
      *
-     * @param shopcode
-     * @param wxpingzheng
+     * @param shopcode shopcode
+     * @param wxpingzheng 微信凭证
      * @return
-     * @throws Exception
      */
     public String getWeiXiuYuanCodesStr(String shopcode, String wxpingzheng) {
         String rtnStr = "";
