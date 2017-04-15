8c01d1bc7d015bdc640f2a84244152201966df7b
lizhen <724577807@qq.com>
2017-04-15 15:55:42 +0800

    修改配置

--- src/main/java/com/dudu/weixin/util/Constant.java
+++ src/main/java/com/dudu/weixin/util/Constant.java
@@ -8,18 +8,18 @@ public final class Constant {
     /**
      * 微信开发者的appid
      */
-//    public static final String APPID = "wxf0af72edbe855d28"; //嘟嘟车网测试平台的
-    public static final String APPID = "wxd4e76e01e4a6e3b7"; //个人测试使用的
+    public static final String APPID = "wxf0af72edbe855d28"; //嘟嘟车网测试平台的
+//    public static final String APPID = "wxd4e76e01e4a6e3b7"; //个人测试使用的
     /**
      * 微信开发者的appSecret
      */
-//    public static final String APPSERECT = "fa12f20abeabc7c8ca3ebe777ceb2229"; //嘟嘟车网测试平台的
-    public static final String APPSERECT = "dd1e044b9208d43a5a31238e5ee053c7"; //个人测试使用的
+    public static final String APPSERECT = "fa12f20abeabc7c8ca3ebe777ceb2229"; //嘟嘟车网测试平台的
+//    public static final String APPSERECT = "dd1e044b9208d43a5a31238e5ee053c7"; //个人测试使用的
     /**
      * 微信的url
      */
-//    public static final String URL = "lm.wx.dev.duduchewang.cn"; //嘟嘟车网测试平台的
-    public static final String URL = "4ec43db5.ngrok.io"; //个人测试使用的
+    public static final String URL = "lm.wx.dev.duduchewang.cn"; //嘟嘟车网测试平台的
+//    public static final String URL = "4ec43db5.ngrok.io"; //个人测试使用的
     /**
      * 微信开发者的token
      */
