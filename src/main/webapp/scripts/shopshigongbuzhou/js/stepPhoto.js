cd1dd5be3ca794c426fa3c7f3485f14ae03f6b35
liusw <951450218@qq.com>
2017-05-12 13:50:55 +0800

    实际流程

--- src/main/webapp/scripts/shopshigongbuzhou/js/stepPhoto.js
+++ src/main/webapp/scripts/shopshigongbuzhou/js/stepPhoto.js
@@ -21,7 +21,7 @@ $(function () {
     var ul = $("ul");//-----------------------------------------------------获取步骤容器
     var btn = $(".btn");//---------------------------------------------------获取标准流程按钮
     var plateNumber = $("#plateNumber").val()//----------------------------获取车牌号
-    console.log(plateNumber)
+
     $.ajax({
         type: 'POST',
         url: '/shopAjax',
@@ -34,24 +34,27 @@ $(function () {
         success: function (jsonData) {
             var arr = JSON.parse(jsonData);
             console.log(arr);
-              var title = '';//单个项目中的title
-              for (var i = 0; i < arr.length; i++) {        //循环的项目个数
-                 var arr2 = arr[i];
-                  //循环每个项目并写出相应的车牌号码和项目名称
-                      title+="<div class='title font_1'>" +
-                          "<div class='title_left'>"+
-                          "<span class='step_color1'>车牌号：</span>"+
-                          "<span class='step_color2 car_num'>"+plateNumber+"</span>"+
-                          "</div>"+
-                          "<div class='title_right'>"+
-                          "<span class='step_color2 project_name'>"+arr2[0].projectName+"</span>"+
-                          "<span class='step_color1'>项目：</span>"+
-                          "</div>"+
-                          "</div>"+
-                          "<ul >"
+            var title = '';//单个项目中的title
+            for (var i = 0; i < arr.length; i++) {        //循环的项目个数
+                var arr2 = arr[i];
+                //循环每个项目并写出相应的车牌号码和项目名称
+                title+="<div class='title font_1'>" +
+                    "<div class='title_left'>"+
+                    "<span class='step_color1'>车牌号：</span>"+
+                    "<span class='step_color2 car_num'>"+plateNumber+"</span>"+
+                    "</div>"+
+                    "<div class='title_right'>"+
+                    "<span class='step_color2 project_name'>"+arr2[0].projectName+"</span>"+
+                    "<span class='step_color1'>项目：</span>"+
+                    "</div>"+
+                    "</div>"+
+                    "<ul >"
 
-                  //var html = '';     //循环每个项目的内容
+                var itemCode = ""; //记录数据中的itemCode
+                var shopCodeLm = "";//记录数据中的shopCodeLm
                 for(var j = 0; j < arr2.length; j++){
+                    itemCode = arr2[j].itemCode;
+                    shopCodeLm = arr2[j].shopCodeLm;
                     if(arr2[j].images.length != 0){
                         var srcs = arr2[j].images[0].fileUrl;
                     }else{
@@ -67,33 +70,34 @@ $(function () {
                         "</li>";
 
                 }
-                  title+="</ul>"+
-                          "<div id='btn_idv' class='btn  font_3 color_3'>标准流程</div>"+
-                          "<span class='fgx'></span>"
+                title+="</ul>"+
+                    "<div class='btn  font_3 color_3' itemCode='"+itemCode+"' shopCodeLm='"+shopCodeLm+"'>标准流程</div>"+
+                    "<span class='fgx'></span>"
 
 
             }
             $(".wrap").append(title);
-            $("#btn_idv").on("click", function () {
+            $(".btn").on("click", function () {
+                var codes = $(this).attr("itemCode");
+                var codelm = $(this).attr("shopCodeLm");
                 //TODO 获取参数进行替换
-                window.location.href = "/shopweixinServlet?serviceType=biaozhunliucheng&itemCode=CSXM0001000600010002&shopCodeLm=CS000";
+                window.location.href = "/shopweixinServlet?serviceType=biaozhunliucheng&itemCode="+codes+"&shopCodeLm="+codelm;
             });
 
             var images = $(".images"); //--------------------------------------获取页面中的每一步的图片
-            images.on("click", function () {
-                var src = $(this).attr("src");
-                $(".box").show();
-                $(".img").attr("src", src);
-                $(".box").bind("touchmove", function (e) {
-                    e.preventDefault(); //遮罩层出现后禁止body滑动
-                });
-            })
         }
 
 
     });
 
-
+    images.on("click", function () {
+        var src = $(this).attr("src");
+        $(".box").show();
+        $(".img").attr("src", src);
+        $(".box").bind("touchmove", function (e) {
+            e.preventDefault(); //遮罩层出现后禁止body滑动
+        });
+    })
     $(".box").on("click", function () {
         $(this).hide()
     });
