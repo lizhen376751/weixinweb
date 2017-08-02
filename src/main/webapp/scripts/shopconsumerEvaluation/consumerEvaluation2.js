f862adfade28dac971ed1dd3f878eaf34be175aa
niuss <2317925841@qq.com>
2017-08-02 15:56:03 +0800

    666

--- src/main/webapp/scripts/shopconsumerEvaluation/consumerEvaluation2.js
+++ src/main/webapp/scripts/shopconsumerEvaluation/consumerEvaluation2.js
@@ -29,21 +29,21 @@ $(function(){
         },
         success:function(jsondata){
             var json = JSON.parse(jsondata);
-           console.log(json)
-                //    是否含有消费商品判断
-                if(json.commodityMx.length != 0){
-                    for(var j = 0;j < json.commodityMx.length;j++){
-                        addPJ(json.commodityMx[j].moduleName,json.commodityMx[j].weixiuyuanSp,json.commodityMx[j].spbm,"COMMODITY");
-                    }
+            console.log(json)
+            //    是否含有消费商品判断
+            if(json.commodityMx.length != 0){
+                for(var j = 0;j < json.commodityMx.length;j++){
+                    addPJ(json.commodityMx[j].moduleName,json.commodityMx[j].weixiuyuanSp,json.commodityMx[j].spbm,"COMMODITY");
                 }
-                //    是否含有消费项目判断
-                if(json.projectMx.length != 0){
-                    for(var j = 0;j < json.projectMx.length;j++){
-                        addPJ(json.projectMx[j].itemName,json.projectMx[j].weixiuyuan,json.projectMx[j].itemcode,"PROJECT");
-                    }
+            }
+            //    是否含有消费项目判断
+            if(json.projectMx.length != 0){
+                for(var j = 0;j < json.projectMx.length;j++){
+                    addPJ(json.projectMx[j].itemName,json.projectMx[j].weixiuyuan,json.projectMx[j].itemcode,"PROJECT");
                 }
-                $(".shop_num").attr("OrderType",json.ordertype);//单据类型
-                $(".shop_num").attr("CreateDate",json.kprq);//开票日期
+            }
+            $(".shop_num").attr("OrderType",json.ordertype);//单据类型
+            $(".shop_num").attr("CreateDate",json.kprq);//开票日期
 
         },
         error:function(data){
@@ -63,35 +63,25 @@ $(function(){
             '<ul>'+
             '<li><img src="/files/shopconsumerEvaluation/flower_bg.png" /></li>'+
             '<li><img src="/files/shopconsumerEvaluation/flower_bg.png" /></li>'+
-            '<li><img src="/files/shopconsumerEvaluation/flower_bg.png"/></li>'+
+            '<li><img src="/files/shopconsumerEvaluation/flower_bg.png" /></li>'+
             '<li><img src="/files/shopconsumerEvaluation/flower_bg.png" /></li>'+
             '<li><img src="/files/shopconsumerEvaluation/flower_bg.png" /></li>'+
             '</ul>'+
-            '<input type="hidden"  value="0" />'+
             '</div>'+
             '<div class="project_text">'+
-            '<textarea class="font_4" rows="4" maxlength="110" placeholder="评论：本店的服务满足您的期待吗？请评价一下我们的优点和不足的地方吧!（满足15个字才是 好同志哦！）"></textarea>'+
+            '<textarea class="font_4" rows="4" maxlength="110" disabled="disabled"></textarea>'+
             '<ul>'+
             '<li class="margin_r">'+
-            '<input type="file" class="project_file" />'+
-           // '<img src="/files/shopconsumerEvaluation/add_img.png" class="add_img"/>'+
+            //'<img src="/files/shopconsumerEvaluation/add_img.png" class="add_img"/>'+
             '<img src="" class="file_img"/>'+
-            '<input type="hidden" class="uuid" value="" />'+
-            '<input type="hidden" class="mxid" value="1" />'+
             '</li>'+
             '<li class="margin_r hide">'+
-            '<input type="file" class="project_file" />'+
            // '<img src="/files/shopconsumerEvaluation/add_img.png" class="add_img"/>'+
             '<img src="" class="file_img"/>'+
-            '<input type="hidden" class="uuid" value="" />'+
-            '<input type="hidden" class="mxid" value="2" />'+
             '</li>'+
             '<li class="hide">'+
-            '<input type="file" class="project_file" />'+
-          //  '<img src="/files/shopconsumerEvaluation/add_img.png" class="add_img"/>'+
+           // '<img src="/files/shopconsumerEvaluation/add_img.png" class="add_img"/>'+
             '<img src="" class="file_img"/>'+
-            '<input type="hidden" class="uuid" value="" />'+
-            '<input type="hidden" class="mxid" value="3" />'+
             '</li>'+
             '</ul>'+
             '</div>'+
@@ -109,70 +99,63 @@ $(function(){
         },
         success:function(jsondata){
             var json=JSON.parse(jsondata)
-            var box_llen=$(".box_l");
-            //console.log(json);
+            console.log(json);
             if(json.length>0){
                 for(var ii=0;ii<json.length;ii++){
-                    for(var aaa=0;aaa<box_llen.length;aaa++){
-                        var thiselem=box_llen.eq(aaa);
-                        if(thiselem.attr("commoditycode")==json[ii].commodityCode){
-                            //获取商品或者项目的评价个数
-                            var commodityStarlevel=json[ii].commodityStarlevel;
-                            switch (commodityStarlevel){
-                                case "ONE":flower(thiselem,"project_num",1);
-                                    break;
-                                case "TWO":flower(thiselem,"project_num",2);
-                                    break;
-                                case "THRER":flower(thiselem,"project_num",3);
-                                    break;
-                                case "FOUR":flower(thiselem,"project_num",4);
-                                    break;
-                                case "FIVE":flower(thiselem,"project_num",5);
-                                    break;
+                    var thiselem=$("div[commoditycode='"+json[ii].commodityCode+"']");
+                    //获取商品或者项目的评价个数
+                    var commodityStarlevel=json[ii].commodityStarlevel;
+                    switch (commodityStarlevel){
+                        case "ONE":flower(thiselem,"project_num",1);
+                            break;
+                        case "TWO":flower(thiselem,"project_num",2);
+                            break;
+                        case "THRER":flower(thiselem,"project_num",3);
+                            break;
+                        case "FOUR":flower(thiselem,"project_num",4);
+                            break;
+                        case "FIVE":flower(thiselem,"project_num",5);
+                            break;
 
-                            };
-                            //获取店铺的评价个数
-                            var shopStarlevel=json[0].shopStarlevel;
-                            switch (shopStarlevel){
-                                case "ONE":shopflower("shop_num",1);
-                                    break;
-                                case "TWO":shopflower("shop_num",2);
-                                    break;
-                                case "THRER":shopflower("shop_num",3);
-                                    break;
-                                case "FOUR":shopflower("shop_num",4);
-                                    break;
-                                case "FIVE":shopflower("shop_num",5);
-                                    break;
+                    };
+                    //获取店铺的评价个数
+                    var shopStarlevel=json[0].shopStarlevel;
+                    switch (shopStarlevel){
+                        case "ONE":shopflower("shop_num",1);
+                            break;
+                        case "TWO":shopflower("shop_num",2);
+                            break;
+                        case "THRER":shopflower("shop_num",3);
+                            break;
+                        case "FOUR":shopflower("shop_num",4);
+                            break;
+                        case "FIVE":shopflower("shop_num",5);
+                            break;
 
-                            }
-                            //获取评价的内容
-                            thiselem.find(".project_text").find("textarea").text(json[ii].commodityContent);
-                            //获取图片
-                            var nnn=json[ii].imgs;
-                            if(nnn.length>0){
-                                for(var imglen=0;imglen<nnn.length;imglen++){
-                                    thiselem.find(".project_text").find("li").eq(imglen).find("img").attr("src",nnn[imglen]);
-                                }
-                            };
-                        }
                     }
-                 //   var thiselem=$("div[commoditycode='"+json[ii].commodityCode+"']");
-
+                    //获取评价的内容
+                    thiselem.find(".project_text").find("textarea").text(json[ii].commodityContent);
+                    //获取图片
+                    var nnn=json[ii].imgs;
+                    if(nnn.length>0){
+                        for(var imglen=0;imglen<nnn.length;imglen++){
+                            thiselem.find(".project_text").find("li").eq(imglen).find("img").attr("src",nnn[imglen]);
+                        }
+                    };
+                }
             }
-          }
 
         }
     });
 
     function flower(elem,name,num){    //商品的评价
 
-            elem.find("."+name).find("li").each(function(index,element){
-                 if(index<num){
-                     $(this).find("img").attr("src","/files/shopconsumerEvaluation/flower_gb.png");
-                 }
-            })
-        }
+        elem.find("."+name).find("li").each(function(index,element){
+            if(index<num){
+                $(this).find("img").attr("src","/files/shopconsumerEvaluation/flower_gb.png");
+            }
+        })
+    }
     function shopflower(name,num){    //店铺的评价
 
         $(".shop_num").find("li").each(function(index,element){
