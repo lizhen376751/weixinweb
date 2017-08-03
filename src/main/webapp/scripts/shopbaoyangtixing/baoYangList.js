857d2020f1477329c22e0d1c1d13c16cc40ad9c2
niuss <2317925841@qq.com>
2017-08-03 11:20:59 +0800

    22

--- src/main/webapp/scripts/shopbaoyangtixing/baoYangList.js
+++ src/main/webapp/scripts/shopbaoyangtixing/baoYangList.js
@@ -140,7 +140,7 @@ $(document).ready(function () {
                             }else{
                                 bylx2 =  json.rows[ii].demandTypeName;
                             }
-
+                         alert(bylx2)
                             html += "<li>" +
                                 "<div class='by_center by_top'>" +
                                 "<span class='by_date font_2'>" + data + "</span>" +
