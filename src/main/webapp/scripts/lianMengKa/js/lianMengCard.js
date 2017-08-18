97b0add2770969edc22e3f2d27ca19d97d396556
niuss <2317925841@qq.com>
2017-08-18 19:03:35 +0800

    qq

--- src/main/webapp/scripts/lianMengKa/js/lianMengCard.js
+++ src/main/webapp/scripts/lianMengKa/js/lianMengCard.js
@@ -71,42 +71,18 @@ $(document).ready(function () {
     var body = $("body");
 	//向html中添加数据
 	function appenging (arr) {
-		//console.log(arr)
 		if(arr!=null && arr.length>0){
+
 			for (var i = 0; i < arr.length;i++) {
-				obj_append("div1","lianMeng",body,"div","");
-				//卡的头部
-				obj_append("div2","box",$($(".lianMeng")[i]),"div","");
-				//卡的名字
-				obj_append("div3","card_name font_3",$($(".box")[i]),"div",arr[i].cardName);
-				//卡的详情
-				obj_append("div4","card_content",$($(".box")[i]),"div","");
-				//条形码
-				obj_append("div5","card_barcode",$($(".card_content")[i]),"div","");
-				obj_append("div6","barcode_img",$($(".card_barcode")[i]),"img","");
-				//生成条形码
-                $($('.card_content .card_barcode .barcode_img')[i]).JsBarcode(arr[i].cardNumber,options);//jQuery
-				//卡号
-				obj_append("div7","card_number font_4",$($(".card_content")[i]),"div","");
-				obj_append("div8","",$($(".card_number")[i]),"p","您的卡号为：");
-				obj_append("div9","number",$($(".card_number")[i]),"div","");
-				obj_append("div10","first",$($(".number")[i]),"span",arr[i].cardNumber);
-				//卡内剩余
-				obj_append("div11","card_surplus",$($(".lianMeng")[i]),"div","");
-				obj_append("div12","font_4",$($(".card_surplus")[i]),"p","卡内剩余");
-				//ul及li
-				obj_append("div13","font_4",$($(".card_surplus")[i]),"ul","");
-				obj_append("div14","biaoTou",$($("ul")[i]),"li","");
-				//第一部分
-				obj_append("div16","width_2 border_1 height",$($(".biaoTou")[i]),"span","项目");
-				obj_append("div17","width_2 margin_2 border_1 height",$($(".biaoTou")[i]),"span","有效期");
-				obj_append("div18","width_2 margin_2 border_1 height",$($(".biaoTou")[i]),"span","次数");
-                var s = 0;
-				for (var j = 0;j < arr[i].leftMx.length;j++) {
-					var yxrqDateTime = arr[i].leftMx[j].effectiveDate;
+                var htmlemle='';
+                var htmlist='';
+                var htmlijhuo='';
+
+                for (var j = 0;j < arr[i].leftMx.length;j++) {
+                    var yxrqDateTime = arr[i].leftMx[j].effectiveDate;
                     var dates = dateFormat(yxrqDateTime);
                     var cc = dates.replace(/-/ig,"/");
-					var news = new Date();
+                    var news = new Date();
                     var e = news.getFullYear();
                     var f = news.getMonth()+1;
                     var g = news.getDate();
@@ -114,27 +90,18 @@ $(document).ready(function () {
                     // console.log(h);
                     var newDate = new Date(h);
                     var oldDate = new Date(cc);
-					if(arr[i].leftMx[j].currentNum != 0 && arr[i].leftMx[j].currentNum != ""){
+                    if(arr[i].leftMx[j].currentNum != 0 && arr[i].leftMx[j].currentNum != ""){
                         if(oldDate >= newDate){
-                            obj_append("div14","border_2",$($("ul")[i]),"li","");
-                            s++;
-                            var li = $($("ul")[i]).find("li");
-                            //obj_append("div15","width_1",$(li[s]),"span",s);
-                            obj_append("div16","width_2",$(li[s]),"span",arr[i].leftMx[j].spName);
-                            obj_append("div17","width_2 margin_2",$(li[s]),"span",dates);
-                            obj_append("div18","width_2 margin_2",$(li[s]),"span",arr[i].leftMx[j].currentNum);
-                            obj_append("div15","width_1 margin_1",$(li[s]),"img","","/files/lianMengKa/img/erweima.png",arr[i].leftMx[j].cardId,arr[i].leftMx[j].itemCode,arr[i].leftMx[j].typeFlg);
+                            htmlist+='<li class="border_2">' +
+									      '<span class="width_2 font_4">'+arr[i].leftMx[j].spName+'</span>'+
+                                          '<span class="width_2 font_4 margin_2">'+dates+'</span>'+
+                                          '<span class="width_2 font_4 margin_2">'+arr[i].leftMx[j].currentNum+'</span>'+
+                                          '<img class="width_1 margin_1"    src="/files/lianMengKa/img/erweima.png" card_id="'+arr[i].leftMx[j].cardId+'"  item_code="'+arr[i].leftMx[j].itemCode+'"  type_flg="'+arr[i].leftMx[j].typeFlg+'">'+
+
+								     '</li>'
                         }
-					}
-				}
-                //------------------------------------------------------------------------判断当前联盟卡里是否还有项目
-                var cars_lis = $($("ul")[i]).find("li");
-                if(cars_lis.length == 1){
-                    cars_lis.parents(".lianMeng").hide();
+                    }
                 }
-				//添加明细
-				obj_append("div19","card_detailed font_3",$($(".card_surplus")[i]),"a","明细");
-                $($("a")[i]).attr("href","/oauthLoginServlet?flagStr=lianMengDetails&cardName="+arr[i].cardName+"&cardNo="+arr[i].cardNumber)
                 //判断是否要开工单
                 $.ajax({
                     type: 'POST',
@@ -144,22 +111,58 @@ $(document).ready(function () {
                     },
                     success: function (jsonData) {
                         var arr = JSON.parse(jsonData);
-                        console.log(arr)
-						if(arr.statusEnum=="BLANK"){
-                        	alert(11)
-                            //允许开单
-                            obj_append("div21","card_detailed font_3",$($(".card_surplus")[i]),"a","开单激活");
-                            $($("a")[i]).attr("href","/lmInternalJump?business=selfbilling")
-						}else{
+                       alert(arr.statusEnum)
+                        if(arr.statusEnum=="BLANK"){
 
-						}
-                        //obj_append("div21","card_detailed font_3",$($(".card_surplus")[i]),"a","明细");
-                       // $($("a")[i]).attr("href","/getCommonAjax?fromflag=billing"+arr[i].cardName+"&cardNo="+arr[i].cardNumber)
+                            //允许开单
+                            htmlijhuo+='<a class="card_detailed font_3" href="/lmInternalJump?business=selfbilling">开单激活</a>'
+
+                        }
+
                     }
                 });
 
-                //添加每一张卡与卡之间的分界条
-                obj_append("div20","fjt",$($(".lianMeng")[i]),"span");
+                htmlemle+='<div class="lianMeng">' +
+                               '<div class="box">' +
+						          '<div class="card_name font_3">'+arr[i].cardName+'</div>'+
+                                  '<div class="card_content">' +
+						               '<div class="card_barcode">'+
+						                  '<img class="barcode_img jscode_img"    src=" ">'+
+					                    '</div>'+
+                                        '<div class="card_number font_4">'+
+                                               '<p>您的卡号为：</p>'+
+												'<div class="number">'+
+												   '<span class="first">'+arr[i].cardNumber+'</span>'+
+												'</div>'+
+									   '</div>'+
+                                    '</div>'+
+                                  '</div>'+
+								  '<div class="card_surplus">'+
+								      '<p class="font_4">卡内剩余</p>'+
+								      '<ul>'+
+						                  '<li class="biaoTou">'+
+					                             '<span class="width_2 font_4 border_1 height">项目'+
+												 '</span><span class="width_2 font_4 margin_2 border_1 height">有效期'+
+					                             '</span><span class="width_2 font_4 margin_2 border_1 height">次数</span>'+
+										   '</li>'+
+                                             htmlist+
+					                  '</ul>'+
+						              '<a class="card_detailed font_3" href="/oauthLoginServlet?flagStr=lianMengDetails&cardName="'+arr[i].cardName+'"&cardNo="'+arr[i].cardNumber+'">明细</a>'+
+                                       htmlijhuo+
+							     '</div>'+
+                                  '<span class="fjt"></span>'+    //添加每一张卡与卡之间的分界条
+					      '</div>'
+                $("body").append(htmlemle);
+				$(".jscode_img").JsBarcode(arr[i].cardNumber,options);
+
+                //------------------------------------------------------------------------判断当前联盟卡里是否还有项目
+                var cars_lis = $($("ul")[i]).find("li");
+                if(cars_lis.length == 1){
+                    cars_lis.parents(".lianMeng").hide();
+                }
+				//添加明细
+
+
 			}
 		}else{
 			alert("当前车辆无联盟卡信息！");
